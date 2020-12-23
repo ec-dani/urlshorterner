d6707b7fbc8c66338a27fb56cd0d8b01fa5c410f
ec-dani <daniel.cabezas1899@gmail.com>
2020-12-24 00:14:57 +0100

    types añadido, _id a  models y UserInDb

--- urlshorterner/main.py
+++ urlshorterner/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI,Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from starlette.responses import RedirectResponse
-from urlshorterner.models import User,Link
+from urlshorterner.models import User,Link,UserInDB
 from pymongo import MongoClient
 from bson import json_util
 import json
@@ -17,9 +17,6 @@ users_col= db['users']
 links_col = db['links']
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-
-class UserInDB(User):
-    hashed_password: str
 
 def fake_hash_password(password: str):
     return "ec_" + password
@@ -50,13 +47,16 @@ async def add_a_url(link: Link):
 
 @app.get("/urls")
 async def show_all_urls():
-  urls =json.loads(json_util.dumps(list(links_col.find())))
-  if not urls:
+  links = []
+  for link in links_col.find():
+    links.append(Link(**link))
+  if not links:
     return {"message": "mas solo que la una"}
-  return urls
+  return links
 
+#Usar UserInDB
 @app.post("/user")
-async def add_a_user(user: User):
+async def add_a_user(user: UserInDB):
   users_col.insert_one({
     'username': user.username,
     'hashed_password': user.hashed_password,
@@ -66,17 +66,22 @@ async def add_a_user(user: User):
 
 @app.get("/users")
 async def show_all_users():
-  users = list(users_col.find())
-  return json.loads(json_util.dumps(users))
+  users = []
+  for user in users_col.find():
+    users.append(User(**user))
+  if not users:
+    return {"message": "mas solo que la una"}
+  return users
+ # users = list(users_col.find())
+ # return json.loads(json_util.dumps(users))
 
 
 @app.get("/url/{furl_shorter}")
 async def go_to(furl_shorter: str):
   short = links_col.find_one({"url_shorter": furl_shorter })
-  jshort = json.loads(json_util.dumps(short))
   if short is None:
     return {"message":"no encontrado"}
-  return RedirectResponse(url=jshort["url"] )
+  return RedirectResponse(url=short["url"] )
 
 @app.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
@@ -86,7 +91,7 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
   user = UserInDB(**user_dict)
   hashed_password = form_data.password
   if not hashed_password == user.hashed_password:
-    raise HTTPException(status_code=400, detail="Contraseña incorrecta")
+    raise HTTPException(status_code= 400, detail="Contraseña incorrecta")
   return {"access_token": user.username, "token_type": "bearer"}
 
 @app.get("/secreto")
--- urlshorterner/models.py
+++ urlshorterner/models.py
@@ -1,12 +1,23 @@
 from pydantic import BaseModel
 from typing import Optional
-
+from urlshorterner.types import PyObjectId
+from bson import ObjectId
 class Link(BaseModel):
+  _id: PyObjectId
   url: str
   url_shorter: Optional[str] = None
+  class Config:
+    arbitrary_types_allow = True
+    json_encoders = {ObjectId: str}
 
 
 class User(BaseModel):
-    username: str
+  _id: PyObjectId
+  username: str
+  full_name: Optional[str] = None
+  class Config:
+    arbitrary_types_allow = True
+    json_encoders = {ObjectId: str}
+
+class UserInDB(User):
     hashed_password: str
-    full_name: Optional[str] = None
